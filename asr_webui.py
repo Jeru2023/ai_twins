286585b208112b1c17b1e70a4bfdb66011b9f8f9
liujeru <jeru.token@gmail.com>
2024-02-04 21:51:43 +0800

    update asr_webui.py

--- asr_webui.py
+++ asr_webui.py
@@ -6,18 +6,11 @@ import os
 root_path = utils.get_root_path()
 asr_model = ASRModel()
 
-
-def audio_transcription(file_obj):
-    wav_file_path = file_obj.name
-    if not wav_file_path:
-        return "Please upload a .wav audio file"
-    text = asr_model.inference(wav_file_path)
-    return text
-
+upload_folder = os.path.join(root_path, "output/upload/")
+upload_file_path = ''
 
 title = "ASR Demo"
-description = "transcribe .wav to text"
-
+description = "To transcribe .wav to text with FunASR model."
 
 sample1 = os.path.join(root_path, "data/audio/sample_short.wav")
 sample2 = os.path.join(root_path, "data/audio/sample_long.wav")
@@ -28,18 +21,6 @@ examples = [
 ]
 
 
-# inputs = gr.File(label='Upload a .wav audio file', file_types=[".wav"])
-#
-# demo = gr.Interface(
-#     fn=audio_transcription,
-#     inputs=inputs,
-#     outputs="text",
-#     examples=examples,
-#     title=title,
-#     description=description,
-#     theme=gr.themes.Soft(),
-# )
-
 def toggle(choice):
     if choice == "file":
         return gr.update(visible=True), gr.update(visible=False)
@@ -47,19 +28,50 @@ def toggle(choice):
         return gr.update(visible=False), gr.update(visible=True)
 
 
+def upload(file_path):
+    global upload_file_path
+    upload_file_path = file_path
+
+
+def audio_transcription(file_path):
+    if not file_path:
+        return "Please upload a .wav audio file"
+
+    text = asr_model.inference(file_path)
+    return text
+
+
 with gr.Blocks() as demo:
+    with gr.Row():
+        gr.Markdown(f'<H1>{title}</H1>')
+    with gr.Row():
+        gr.Markdown(description)
     with gr.Row():
         with gr.Column():
             r = gr.Radio(["file", "mic"], value="file", label="How would you like to upload your audio?")
-            m = gr.Mic(label="Input")
-            f = gr.Audio(type="filepath", label="Input", visible=False)
+            f = gr.Audio(type="filepath", label="Input Audio")
+            m = gr.Mic(label="Input Audio", visible=False)
+
+            gr.Markdown(' ')
+
+            vs_checker = gr.Checkbox(label="Enable Vocal Separation", info="checked if audio is noisy")
+
+            as_checker = gr.Checkbox(label="Enable Audio Slice", info="checked if audio is long")
+            submit_btn = gr.Button("Submit")
+
         with gr.Column():
-            output = gr.Audio(label="Output")
+            output = gr.TextArea(label="Transcription Output")
 
-    r.change(toggle, r, [m, f])
-    m.change(lambda x: x, m, output)
-    f.change(lambda x: x, f, output)
+    r.change(toggle, r, [f, m])
+    f.change(upload, f)
+    m.change(upload, m)
 
-demo.launch()
+    if r.value == "file":
+        mode_inputs = [f]
+    else:
+        mode_inputs = [m]
 
+    submit_btn.click(fn=audio_transcription, inputs=mode_inputs, outputs=output)
+
+demo.theme = gr.themes.Soft()
 demo.launch(server_name='localhost')
